2c0e4404568673f84daaf1995c7690bd23e0ffa1
aleksejzuravlev <alecszhuravlev@yandex.ru>
2026-02-05 19:01:15 +0300

    c4

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 public class Flashlight : MonoBehaviour
 {
     public GameObject lightObject;
+    Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        string clipName = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if(lightObject.activeSelf == true)
+            if(clipName == "idal")
+            {
+                if(lightObject.activeSelf == true)
             {
                 lightObject.SetActive(false);
             }
@@ -22,6 +26,7 @@ public class Flashlight : MonoBehaviour
             else
             {
                 lightObject.SetActive(true);
+            }
             }
         }
     }
